60eb601ae9562d037e5bc5fdcedd1f696eca9197
GlebKushnir <fujiphoto42@gmail.com>
2024-12-17 02:12:29 +0200

    lesson3_homework

---
+++ Lessons/ls_3.py
@@ -0,0 +1,61 @@
+# n1, n2 = 10, 20
+# print(n1 > n2)
+# print(n1 >= n2)
+
+# print(1 == 1 and 3 == 3)
+# print(1 == 1 or 2 == 3)
+# print(n1 < n2)
+
+# v1
+# hours = int(input("Enter hours: "))
+
+# if hours >= 12:
+#     print("PM")
+# else:
+#     print("AM")
+
+# v2
+# hours = int(input("Enter hours: "))
+
+# if 12 <= hours < 24:
+#     print("PM")
+# elif 0 <= hours < 12:
+#     print("AM")
+# else:
+#     print("Incorrect hours!")
+
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# number3 = int(input("Enter third number: "))
+
+# if number1 <= number2 <= number3:
+#     print(number1)
+# elif number2 <= number1 <= number3:
+#     print(number2)
+# elif number3 <=number1 <= number2:
+#     print(number3)
+
+# user_select = int(input("1. Start\n2. Settings\n3. Saved game\n. Exit\nSelect your choice: "))
+
+# if user_select == 1:
+#     print("Starting game...")
+# elif user_select == 2:
+#     print("Settings")
+# elif user_select == 3:
+#     print("Saved game")
+# elif user_select == 4:
+#     print("Exit")
+# else:
+#     print("Invalid input, please try again")
+
+# match user_select
+
+
+
+# #  //// LIST
+# number = []
+# number_1 = list()
+# print(type(number))
+# print(type(number_1))
+
+# numbers = [1, 3, 25, 7, 2]
